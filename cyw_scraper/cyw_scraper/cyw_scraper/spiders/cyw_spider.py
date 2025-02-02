d8677e42c4d34b5736f0e8072d066c921f86becf
ivand <ivan.d.kostov@gmail.com>
2025-02-02 21:08:31 +0200

    added: scraper start

---
+++ cyw_scraper/cyw_scraper/cyw_scraper/spiders/cyw_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class HotWheelsSpider(scrapy.Spider):
+    year = '2012'
+
+    name = "cyw-scraper"
+    start_urls = [
+        f"https://hotwheels.fandom.com/wiki/List_of_{year}_Hot_Wheels"
+    ]
+
+    def parse(self, response):
+        # Collect all links under <span class="mw-headline"> then <b> then <a>
+        link_paths = response.css("span.mw-headline b a::attr(href)").getall()
+        for path in link_paths:
+            # Convert relative URL to absolute URL
+            full_url = response.urljoin(path)
+            yield {
+                'title': response.css("h1::text").get(),
+                "url": full_url,
+            }
