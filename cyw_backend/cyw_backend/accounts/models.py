87e038b5d40276b0027f4ae857cb46644bb452b6
ivand <ivan.d.kostov@gmail.com>
2025-02-02 18:31:04 +0200

    updated: user model

--- cyw_backend/cyw_backend/accounts/models.py
+++ cyw_backend/cyw_backend/accounts/models.py
@@ -4,5 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(
-        unique=True,
+        unique=True,)
+
+    collected_cars = models.ManyToManyField(
+        to='cars.Car',
+        related_name='collectors',
+        blank=True,
+        verbose_name='Collected Cars'
     )
+
+    wishlisted_cars = models.ManyToManyField(
+        to='cars.Car',
+        related_name='wishlisters',
+        blank=True,
+        verbose_name='Wishlisted Cars'
+    )
+
+    def __str__(self):
+        return self.username
