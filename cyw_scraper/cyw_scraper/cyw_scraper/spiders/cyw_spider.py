b9a83a794c02b85e6557daf6bfa45f079cb3a8d1
ivand <ivan.d.kostov@gmail.com>
2025-02-05 21:03:26 +0200

    added: car scraper almost done

--- cyw_scraper/cyw_scraper/cyw_scraper/spiders/cyw_spider.py
+++ cyw_scraper/cyw_scraper/cyw_scraper/spiders/cyw_spider.py
@@ -16,22 +16,36 @@ class WheelsSpider(scrapy.Spider):
             full_url = response.urljoin(path)
             yield scrapy.Request(full_url, callback=self.parse_series_details)
 
-
     def parse_series_details(self, response):
-        series_title = response.css("h1::text").get()  # series page title
+        series_title = response.css("#firstHeading span::text").get()
+        rows = response.css("table.wikitable tbody tr")
+        color_number = 1
+        previous_model = ''
 
-        # Select all rows in the wikitable, skipping the header row
-        rows = response.css("table.wikitable tr")[1:]
         for row in rows:
             toy_number = row.css("td:nth-child(1)::text").get()
-            model = row.css("td:nth-child(2)::text").get()
-            series_number = row.css("td:nth-child(3)::text").get()
-            image_url = row.css("td:nth-child(4) img::attr(src)").get()
+            model = row.css("td:nth-child(3) a::text").get()
+            series_number = row.css("td:nth-child(5)::text").get()
+            image_url = row.css("td:nth-child(6) a img::attr(src)").get()
+
+            if not toy_number or not model:
+                continue
+
+            current_model = model
+            if current_model == previous_model:
+                color_number += 1
+                current_model = f"{current_model} / Color: {color_number}"
+            else:
+                color_number = 1
+            previous_model = model
+
+            if image_url and image_url.startswith("data:"):
+                image_url = None
 
             yield {
                 "series_title": series_title,
-                "toy_number": toy_number.strip() if toy_number else None,
-                "model": model.strip() if model else None,
-                "series_number": series_number.strip() if series_number else None,
+                "toy_number": toy_number.strip(),
+                "model": current_model.strip(),
+                "series_number": series_number.split("/")[0],
                 "image_url": image_url,
             }
