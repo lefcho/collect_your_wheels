3ca839254817f93ba43697401f3ca9f2799df507
ivand <ivan.d.kostov@gmail.com>
2025-02-11 01:07:19 +0200

    fixed: user ModelSerializer from basic serializer

--- cyw_backend/cyw_backend/accounts/serializers.py
+++ cyw_backend/cyw_backend/accounts/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 User = get_user_model()
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', ]
