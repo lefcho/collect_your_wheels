ef85cfcad3574f0055c3e830fffb18e9abb4db19
ivand <ivan.d.kostov@gmail.com>
2025-02-17 00:20:14 +0200

    added: collected cars start

--- cyw_frontend/src/pages/CollectedCars/CollectedCars.jsx
+++ cyw_frontend/src/pages/CollectedCars/CollectedCars.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function CollectedCars() {
-  return (
-    <div>CollectedCars</div>
-  )
+
+    return (
+        <div>
+            <h1>Collected Cars</h1>
+            <form >
+                <input 
+                    type="text"
+                    placeholder='Search collected cars...'
+                 />
+            </form>
+        </div>
+    )
 }
 
-export default CollectedCars
+export default CollectedCars;
