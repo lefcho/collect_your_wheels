065aae0d30b18eb5a8ffa7f9aecbf754a5935730
ivand <ivan.d.kostov@gmail.com>
2025-02-22 20:51:55 +0200

    added: Header  styling done

--- cyw_frontend/src/components/Header/Header.jsx
+++ cyw_frontend/src/components/Header/Header.jsx
@@ -1,19 +1,16 @@
-
-import styles from './Header.module.scss';
 import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../utils/logout';
 import { AuthContext } from '../../contexts/AuthContext';
-import logo from '../../assets/logo.svg'
-
+import { motion, AnimatePresence } from 'framer-motion';
+import logo from '../../assets/logo.svg';
+import styles from './Header.module.scss';
 
 function Header() {
     const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
-
     const navigate = useNavigate();
-
     const [query, setQuery] = useState('');
-    const [showProfCont, setShowProfCont] = useState(true);
+    const [showProfCont, setShowProfCont] = useState(false);
 
     const handleLogout = () => {
         logout();
@@ -24,31 +21,32 @@ function Header() {
     const handleSearchSubmit = (e) => {
         e.preventDefault();
         navigate(`/results?search_query=${encodeURIComponent(query)}`);
-    }
+    };
 
     return (
         <header className={styles.header}>
-
             <Link to="/">
                 <div>
                     <img className={styles.logo} src={logo} alt="Logo" />
                 </div>
             </Link>
-            <form
-                className={styles['search-form']}
-                onSubmit={(e) => handleSearchSubmit(e)}>
+            <form className={styles['search-form']} onSubmit={handleSearchSubmit}>
                 <input
                     className={styles['search-input']}
                     value={query}
+                    type="text"
                     placeholder="Search . . ."
                     onChange={(e) => setQuery(e.target.value)}
                 />
-                {query && <button
-                    type='reset'
-                    className={styles['cancel-btn']}
-                    onClick={() => setQuery('')}>
-                    <i className="fa-solid fa-xmark"></i>
-                </button>}
+                {query && (
+                    <button
+                        type="reset"
+                        className={styles['cancel-btn']}
+                        onClick={() => setQuery('')}
+                    >
+                        <i className="fa-solid fa-xmark"></i>
+                    </button>
+                )}
                 <button className={styles['search-btn']} type="submit">
                     <i className="fa-solid fa-magnifying-glass"></i>
                 </button>
@@ -58,31 +56,37 @@ function Header() {
                     <div className={styles['prof-cont']}>
                         <button
                             className={styles['prof-button']}
-                            onClick={() =>
-                                setShowProfCont(!showProfCont)}>
+                            onClick={() => setShowProfCont((prev) => !prev)}
+                        >
                             <i className="fa-solid fa-user"></i>
                         </button>
-                        {showProfCont && <div
-                            className={styles['prof-info-cont']}>
-                            <Link 
-                                className={styles['icon-text']} 
-                                to="/collected">
-                                <i className="fa-solid fa-car"></i>
-                                <button>Collected Cars</button>
-                            </Link>
-                            <Link 
-                                className={styles['icon-text']} 
-                                to="/wishlisted">
-                                <i className="fa-solid fa-car-on"></i>
-                                <button>Wishlisted Cars</button>
-                            </Link>
-                            <button
-                                className={`${styles['icon-text']} ${styles['add-before']}`}
-                                onClick={handleLogout}>
-                                <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                                <p>Logout</p>
-                            </button>
-                        </div>}
+                        <AnimatePresence>
+                            {showProfCont && (
+                                <motion.div
+                                    className={styles['prof-info-cont']}
+                                    initial={{ opacity: 1, y: -50 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -50 }}
+                                    transition={{ duration: 0.3 }}
+                                >
+                                    <Link className={styles['icon-text']} to="/collected">
+                                        <i className="fa-solid fa-car"></i>
+                                        <button>Collected Cars</button>
+                                    </Link>
+                                    <Link className={styles['icon-text']} to="/wishlisted">
+                                        <i className="fa-solid fa-car-on"></i>
+                                        <button>Wishlisted Cars</button>
+                                    </Link>
+                                    <button
+                                        className={`${styles['icon-text']} ${styles['add-before']}`}
+                                        onClick={handleLogout}
+                                    >
+                                        <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                                        <p>Logout</p>
+                                    </button>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 ) : (
                     <Link to="/login">
