e7e845975dfc040a216e3716bc0bd73c67cbd2ba
ivand <ivan.d.kostov@gmail.com>
2025-03-01 21:43:16 +0200

    added: sm car card styling

--- cyw_frontend/src/components/SmallCarCard/SmallCarCard.jsx
+++ cyw_frontend/src/components/SmallCarCard/SmallCarCard.jsx
@@ -1,7 +1,9 @@
 
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
+import styles from './SmallCarCard.module.scss';
+import { s } from 'framer-motion/client';
 
 
 function SmallCarCard(props) {
@@ -17,36 +19,55 @@ function SmallCarCard(props) {
     const { isAuthenticated } = useContext(AuthContext);
 
     return (
-        <div>
-            <h5>
-                <span>{car.series_number}</span>
-                <p>{car.model}</p>
-                <span>{car.toy_number}</span>
-            </h5>
-            {car.is_treasure_hunt && <p>
-                Treasure Hunt
-            </p>}
-            {car.is_super_treasure_hunt && <p>
-                Super Treasure Hunt
-            </p>}
+        <div className={styles['sm-car-card']}>
+            <div className={styles['car-info-cont']}>
+                <h5 className={styles['car-info']}>
+                    <span
+                        className={styles['series-number']}>
+                        {car.series_number}
+                    </span>
+                    <div className={styles['car-name']}>
+                        <p>{car.model}</p>
+                        <span className={styles['toy-number']}>
+                            ({car.toy_number})
+                        </span>
+                    </div>
+                </h5>
+                {car.is_treasure_hunt && <p
+                    className={styles['treasure-hunt']}>
+                    Treasure Hunt
+                </p>}
+                {car.is_super_treasure_hunt && <p
+                    className={styles['super-treasure-hunt']}>
+                    Super Treasure Hunt
+                </p>}
+            </div>
             {isAuthenticated ?
-                <div className="action-buttons">
+                <div className={styles["action-buttons"]}>
                     <button>
                         {car.is_collected ?
-                            <span onClick={() => handleRemoveCollected(car.id)}>
+                            <span
+                                className={styles.button}
+                                onClick={() => handleRemoveCollected(car.id)}>
                                 <i className="fa-solid fa-square-check"></i>
                             </span> :
-                            <span onClick={() => handleAddCollected(car.id)}>
+                            <span
+                                className={styles.button}
+                                onClick={() => handleAddCollected(car.id)}>
                                 <i className="fa-regular fa-square-check"></i>
                             </span>
                         }
                     </button>
                     <button>
                         {car.is_wishlisted ?
-                            <span onClick={() => handleRemoveWishlisted(car.id)}>
+                            <span
+                                className={styles.button}
+                                onClick={() => handleRemoveWishlisted(car.id)}>
                                 <i className="fa-solid fa-heart"></i>
                             </span> :
-                            <span onClick={() => handleAddWishlisted(car.id)}>
+                            <span
+                                className={styles.button}
+                                onClick={() => handleAddWishlisted(car.id)}>
                                 <i className="fa-regular fa-heart"></i>
                             </span>}
                     </button>
