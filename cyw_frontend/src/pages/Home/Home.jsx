6420b52286bfbbf02780c6559566e6297226d872
ivand <ivan.d.kostov@gmail.com>
2025-03-04 21:03:38 +0200

    added: text to home page

--- cyw_frontend/src/pages/Home/Home.jsx
+++ cyw_frontend/src/pages/Home/Home.jsx
@@ -19,6 +19,7 @@ function Home() {
             </video>
             <div className={styles['text-content']}>
                 <h1>Collect Your Wheels</h1>
+                <p>Keep track of your HotWheels collection</p>
                 {isAuthenticated ?
                     <Link to={{ pathname: "/collected" }}>
                         <button className={styles['welcome-btn']}>
