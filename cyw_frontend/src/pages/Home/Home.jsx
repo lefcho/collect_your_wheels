e92a8ed87286427fb4685d00545e4668fc1d42ca
ivand <ivan.d.kostov@gmail.com>
2025-03-04 19:23:54 +0200

    added: home page styling

--- cyw_frontend/src/pages/Home/Home.jsx
+++ cyw_frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+
+import React from 'react';
+import videoBg from '../../assets/videoBg.mp4';
+import styles from './Home.module.scss';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className={styles.home}>
+            <div className={styles.overlay}></div>
+            <video className={styles['video-bg']} autoPlay loop muted>
+                <source src={videoBg} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className={styles['text-content']}>
+                <h1>Collect Your Wheels</h1>
+                <button className={styles['welcome-btn']}>
+                    Start Collecting
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
