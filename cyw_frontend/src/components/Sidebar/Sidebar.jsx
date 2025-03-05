ff23a7258151c2f0be7e4dd862e9ad78bab05107
ivand <ivan.d.kostov@gmail.com>
2025-03-05 19:24:23 +0200

    added:  sidebar styling

--- cyw_frontend/src/components/Sidebar/Sidebar.jsx
+++ cyw_frontend/src/components/Sidebar/Sidebar.jsx
@@ -3,10 +3,20 @@ import React from 'react'
 import styles from './Sidebar.module.scss'
 
 
-function Sidebar() {
+function Sidebar(props) {
+    const { toggleSidebar } = props;
+
     return (
         <div className={styles.sidebar}>
-            Sidebar
+            <div className={styles.header}>
+                <button 
+                    onClick={toggleSidebar}
+                    className={styles.cancelBtn}
+                >
+                    <i className="fa-solid fa-xmark"></i>
+                </button>
+                <h2>Collect Your Wheels</h2>
+            </div>
         </div>
     )
 }
