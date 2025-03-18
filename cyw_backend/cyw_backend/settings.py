d82c33aa4565a479babec2aa622126a9f4137560
ivand <ivan.d.kostov@gmail.com>
2025-03-18 19:39:15 +0200

    added:     'AUTH_HEADER_TYPES' to Bearer

--- cyw_backend/cyw_backend/settings.py
+++ cyw_backend/cyw_backend/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
